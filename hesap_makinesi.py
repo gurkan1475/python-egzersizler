38b1dea8837e10addb61ce43c70564e6fa5d9a6c
Gürkan <gurkan14755@gmail.com>
2025-07-23 11:08:41 +0300

    hesap_makinesi.py

---
+++ hesap_makinesi.py
@@ -0,0 +1,4 @@
+sayi1 = int(input("Lütfen Bir Sayı Giriniz: "))
+sayi2 = int(input("Lütfen İkinci Sayıyı Giriniz: "))
+print("Toplama Sonucu: ", (sayi1+sayi2))
+print("Çarpma Sonucu: ", (sayi1*sayi2))
